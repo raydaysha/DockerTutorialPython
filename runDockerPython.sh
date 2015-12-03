dd091522ce7ad13d68785f2023e91ec3567e3906
raydaysha <raydaysha@gmail.com>
2015-12-03 14:35:48 +0000

    Small change to directory listing

--- runDockerPython.sh
+++ runDockerPython.sh
@@ -1,4 +1,4 @@
 
-docker run -d -e VMHOST=52.33.191.42 --name python -p 8000:8000 --link tomcat-maven:tomcat jeffdavisco/polymer-python:latest /local/git/docker-python/run.sh
+docker run -d -e VMHOST=52.33.191.42 --name python -p 8000:8000 --link tomcat-maven:tomcat dockertutorial/polymer-python:latest /local/git/DockerTutorialPython/run.sh
 
 # once running you can identify the IP addr by using: boot2docker ip. Then, use that IP to access port 8080
